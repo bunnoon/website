1cdd9350dd67ba88cd7e6db4456167c0e38aca28
Phuchon Chueapong <phucholony@gmail.com>
2026-01-05 15:18:47 +0700

    create www project
    Create bunnoon application into projects folder for www subdomain

---
+++ projects/www/src/app/app.config.ts
@@ -0,0 +1,18 @@
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners
+} from '@angular/core'
+import { provideHttpClient, withInterceptors } from '@angular/common/http'
+import { provideRouter } from '@angular/router'
+
+import { routes } from './app.routes'
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+
+    provideHttpClient(withInterceptors([])),
+
+    provideRouter(routes)
+  ]
+}
