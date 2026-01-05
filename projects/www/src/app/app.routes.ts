ed47c4697150ab5a7a6edc64e1321dca26d46836
Phuchon Chueapong <phucholony@gmail.com>
2026-01-06 06:52:43 +0700

    create general/home page into www project

--- projects/www/src/app/app.routes.ts
+++ projects/www/src/app/app.routes.ts
@@ -2,4 +2,9 @@ import { Routes } from '@angular/router'
 
 import * as General from './pages/general'
 
-export const routes: Routes = []
+export const routes: Routes = [
+  {
+    path: '',
+    component: General.HomePageComponent
+  }
+]
